13e11d7343b57986fd1e9aee03fa86d6d9c6d007
Yeison Montoya <yeisondiseno@gmail.com>
2020-08-08 00:12:51 -0500

    Enrutado, esto se pine bueno

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import Header from '../template/Header';
+import Home from '../pages/Home';
+import Character from '../pages/Character';
+import Error404 from '../pages/Error404';
+
+
+const routes = {
+    '/': Home,
+    '/:id': Character,
+    '/Contact': 'Contact',
+}
+
+const router =  async () => {
+    const header = null || document.getElementById('header');
+    const content = null || document.getElementById('document');
+
+    header.innerHTML = await Header();
+}
+
+export default router;
+
