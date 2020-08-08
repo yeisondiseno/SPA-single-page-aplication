afe7579f0fc9a44816883b532d4003729bc46d3f
Yeison Montoya <yeisondiseno@gmail.com>
2020-08-07 22:29:16 -0500

    He creado una plantilla para los html literals

---
+++ src/template/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+    const view = `
+        <div class="Header__main">
+            <div class="Header__logo">
+            <h1>
+                <a haref="/">
+                    100tifi.co
+                </a>
+            </h1>
+            </div>
+            <div class="Header__nav">
+            <a href="#/about/">
+                About
+            </a>
+            </div>
+        </div>
+    `;
+    return view;
+}
+
+export default Header;
